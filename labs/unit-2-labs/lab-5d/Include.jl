fa887cb5838f473314b11e9f47fa9b0c7f71a3ff
HaydenSpector <hayden.spector@gmail.com>
2023-03-09 17:26:31 -0500

    summary

--- labs/unit-2-labs/lab-5d/Include.jl
+++ labs/unit-2-labs/lab-5d/Include.jl
@@ -7,7 +7,6 @@ const _PATH_TO_DATA = joinpath(_ROOT, "data")
 using DataStructures # we need this for the Queue data structure
 
 # load my codes -
-include(joinpath(_PATH_TO_SRC, "Types.jl"))
-include(joinpath(_PATH_TO_SRC, "Factory.jl"))
-include(joinpath(_PATH_TO_SRC, "Files.jl"))
+include(joinpath(_PATH_TO_SRC, "Factory.jl"));
+include(joinpath(_PATH_TO_SRC, "Files.jl"));
 include(joinpath(_PATH_TO_SRC, "Parser.jl"))
