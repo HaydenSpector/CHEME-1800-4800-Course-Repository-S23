95e6ffab4f46f2bcba9390e500284e1cfdc01004
HaydenSpector <hayden.spector@gmail.com>
2023-03-18 15:41:19 -0400

    Update Factory.jl

--- PS3/src/Factory.jl
+++ PS3/src/Factory.jl
@@ -14,12 +14,24 @@ end
 
 Internal function that constructs the system matrix for the chemical decay problem
 """
-function _build_system_matrix(N::Int64, κ::Float64, h::Float64)::Array{Float64,2}
-end
+ function _build_system_matrix(N::Int64, κ::Float64, h::Float64)::Array{Float64,2}
+ """
+     # Initialize
+     A = Array{Float64,2}(undef,N,N)
+     fill!(A,0.0)
+     A[1,1] = 1
 
-"""
-    build(type::Type{MyChemicalDecayModel}; 
-        κ::Float64 = 0.0, h::Float64 = 0.0, N::Int64 = 0, Cₒ::Float64 = 0.0) -> MyChemicalDecayModel
+     # main
+     for i in 2:N #horizontal
+         for j in 2:N #vertical
+             if (i == j)
+                A[i,j]= 1
+                A[i,j-1]=(h*k-1)
+            end
+        end
+    end
+    return A
+end
 
 Build an instance of MyChemicalDecayModel and sets the value of the model parameters. 
 Default model parameters are zero
