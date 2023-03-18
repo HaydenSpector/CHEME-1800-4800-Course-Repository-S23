8194497767a887d9f460daa718882129543f1eea
HaydenSpector <hayden.spector@gmail.com>
2023-03-18 16:25:48 -0400

    Summary

--- PS3/Submit 2.jl
+++
@@ -1,7 +0,0 @@
-include("Include.jl")
-
-Cₒ=10.0
-h=0.1
-N=200
-k=10.0
-model=build(MyChemicalDecayModel, N=N, h=h,κ=k,Cₒ=Cₒ)
--- PS3/Submit 3.jl
+++
@@ -1,7 +0,0 @@
-include("Include.jl")
-
-Cₒ=10.0
-h=0.1
-N=200
-k=100.0
-model=build(MyChemicalDecayModel, N=N, h=h,κ=k,Cₒ=Cₒ)
