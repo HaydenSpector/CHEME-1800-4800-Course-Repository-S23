e7e71c5bfe6e6fcc0ace79cb67c8df85acec784c
HaydenSpector <hayden.spector@gmail.com>
2023-03-18 16:31:42 -0400

    summary

--- PS3/Submit_3.jl
+++ PS3/Submit_3.jl
@@ -12,3 +12,9 @@ b = model.b
 x_gs = solve(GaussSeidelIterationSolver(), A, b, zeros(N))
 
 x_jacobi = solve(JacobiIterationSolver(), A, b, zeros(N))
+
+x_direct = inv(A)*b
+
+error_gs = norm(x_gs - x_direct)
+
+error_jacobi = norm(x_jacobi - x_direct)
