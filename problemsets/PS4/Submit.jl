2c3738ec29c8beab32c52a6d60074b583da2e85d
HaydenSpector <113485067+HaydenSpector@users.noreply.github.com>
2023-03-21 14:10:35 -0400

    Update Submit.jl

--- problemsets/PS4/Submit.jl
+++ problemsets/PS4/Submit.jl
@@ -2,8 +2,8 @@
 include("Include.jl")
 
 # load the data -
-path_to_data_file = joinpath(_PATH_TO_DATA, "Synthetic-Numerical-CoV-10k-Fibrinolysis-NoMLabels.csv") 
-(L,X,list_of_measurements) = load_patient_data_file);
+path_to_data_file = joinpath(_PATH_TO_DATA,"Synthetic-Numerical-CoV-10K-Fibrinolysis-NoMLabels.csv") 
+(L,X,list_of_measurements) = load_patient_data_file(path_to_data_file);
 
 # standardize the data -
 XS = standardize(X);
