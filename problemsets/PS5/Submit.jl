53f9909e117bf3590a3a1180db5e150d1f1d40c5
HaydenSpector <113485067+HaydenSpector@users.noreply.github.com>
2023-03-31 18:08:28 -0400

    commit

--- problemsets/PS5/Submit.jl
+++ problemsets/PS5/Submit.jl
@@ -2,4 +2,4 @@
 include("Include.jl")
 
 #load Data
-loading = loaddata(joinpath(_PATH_TO_DATA, "PS5-dataset-w-repeats.csv"))
+loading = loaddataset(joinpath(_PATH_TO_DATA, "PS5-dataset-w-repeats.csv"))
