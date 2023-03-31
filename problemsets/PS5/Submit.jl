7d44c65e28a5e7a4ecc176e4c7d870292e0a88f6
HaydenSpector <113485067+HaydenSpector@users.noreply.github.com>
2023-03-31 18:05:19 -0400

    Summary

--- problemsets/PS5/Submit.jl
+++ problemsets/PS5/Submit.jl
@@ -1,1 +1,5 @@
-# TODO: Fill me in with your program to compute the uknown model parameters from the data
+#include Files
+include("Include.jl")
+
+#load Data
+loading = loaddata(joinpath(_PATH_TO_DATA, "PS5-dataset-w-repeats.csv"))
--- problemsets/PS5/src/Files.jl
+++ problemsets/PS5/src/Files.jl
@@ -5,10 +5,7 @@ Loads the price dataset, returns the data as a DataFrame type.
 See: https://dataframes.juliadata.org/stable/man/getting_started/
 """
 function loaddataset(path::String)::DataFrame
-    
-    # check: in real-life, we check of the data was a legit file
-    # assume to be ok for now
-    
+
     # load the data, and returns this data as a DataFrame
     return CSV.read(path,DataFrame);
 end
