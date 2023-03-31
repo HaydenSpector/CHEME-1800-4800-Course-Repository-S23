3d98e934c1d48b38f69ca484ab81b6252f6b6522
HaydenSpector <113485067+HaydenSpector@users.noreply.github.com>
2023-03-31 18:45:49 -0400

    Update Compute.jl

--- problemsets/PS5/src/Compute.jl
+++ problemsets/PS5/src/Compute.jl
@@ -2,14 +2,14 @@
     build_data_matrix(data::DataFrame) --> Array{Float64,2}
 """
 function build_data_matrix(data::DataFrame)::Array{Float64,2}
-    n = size(100, 1)
-    m = size(1, 3)
+    n = 100
+    m = 4
     d = zeros(n,m)
 
     # Fill in the dynamic programing build_data_matrix
-    for i in 1:(n-1) # avoid errors 
-        for j in 1:(m-1)
-            dm[i][j] = DataFrame[i-1][j-1] # zero indexed
+    for i in 1:(n) # avoid errors 
+        for j in 1:(m)
+            dm[i][j] = DataFrame[i] # one indexed
         end
     end
     return dm
@@ -23,8 +23,8 @@ TODO: Fill me in.
 function build_output_vector(data::DataFrame)::Array{Float64,1}
     n = length(DataFrame)
     ov = zeros(n)
-    for i in 1:(n-1)
-        ov[i] = DataFrame[i-1]
+    for i in 1:(n)
+        ov[i] = DataFrame[i]
     end
 
 end
